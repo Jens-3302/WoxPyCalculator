7b014046374ced1862a3afd75fd81b3893cd72c7
Jens Welsch <21183736-JensWelsch@users.noreply.gitlab.com>
2024-08-20 22:47:05 +0200

    Added custom variable assignments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from math import *
 import re
 import os
+import json
 
 try:
     import pyperclip
@@ -27,18 +28,47 @@ from builtins import *  # Required for division scipy, also allows for pow to be
 ###########################
   
 sqr = lambda x: x ** 2
-
 x = 0
 
-xFilePath = os.environ['TMP'] + os.sep + "wox_pycalc_x.txt"
-if os.path.exists(xFilePath):
+varFilePath = os.environ['TMP'] + os.sep + "wox_pycalc_vars.json"
+variables = []
+
+if os.path.exists(varFilePath):
     try:
-        with open(xFilePath, "r") as xFile:
-            x = float(xFile.read())
+        with open(varFilePath, "r") as varFile:
+            data = json.load(varFile)
+            for key in data:
+                exec(key + " = " + str(data[key]))
+                variables.append(key)
     except:
         pass
 
-  
+def delete(variable):
+    if not isinstance(variable, str):
+        return "Surround with '"
+    try:
+        with open(varFilePath, "r") as varFile:
+            data = json.load(varFile)
+            exists = data.get(variable) is not None
+            del data[variable]
+            with open(varFilePath, "w") as varFile:
+                json.dump(data, varFile)
+                if exists:
+                    return "Deleted"
+                else:
+                    return "Not Found"
+    except:
+        pass
+    return "Didnt work"
+
+def deleteVariables():
+    try:
+        with open(varFilePath, "w") as varFile:
+            json.dump({}, varFile)
+            return "done"
+    except:
+        pass
+
 ############################
 # Pre-calculation handlers #
 ############################
@@ -69,7 +99,7 @@ def handle_factorials(query):
     return query
 
 def handle_pow_xor(query):
-    return query.replace("^", "**").replace("xor", "^")
+    return query.replace("^", "**").replace("²", "**2").replace("³", "**3").replace("xor", "^")
 
 def handle_implied_multiplication(query):
     return re.sub(r'((?:\.\d+|\b\d+\.\d*|\b\d+)(?:[eE][-+]?\d+)?)\s*(x|pi)\b',
@@ -82,6 +112,27 @@ def handle_missing_parentheses(query):
     if parDiff < 0:
         return '('*(-parDiff) + query
     return query
+    
+variableName = 'x'
+
+def handle_assign(query):
+    global variableName
+    if query.count('=') != 1:
+        return query
+    equ = query.split('=', 1)
+    if bool(re.match(r'^[^\W0-9]\w*\s*[\+\-\*\/]?$', equ[0])):
+        if equ[0][-1] in '+-*/':
+            equ[1] = equ[0]+equ[1]
+            equ[0] = equ[0][:-1]
+        variableName = equ[0].strip()
+        return equ[1]
+    if bool(re.match(r'^\s*[^\W0-9]\w*$', equ[1])):
+        if equ[0][-1] in '+-*/':
+            equ[0] = equ[0]+equ[1]
+        variableName = equ[1].strip()
+        return equ[0]
+        
+        
 
 ####################
 # Post Calculation #
@@ -111,11 +162,19 @@ def copy_to_clipboard(text):
         cmd = 'echo ' + text.strip() + '| clip'
         os.system(cmd)
 
-def write_to_x(result):
-    x = result
+def write_to_vars(result, variableName):
     try:
-        with open(xFilePath, "w") as xFile:
-            xFile.write(result)
+        if os.path.exists(varFilePath):
+            with open(varFilePath, "r") as varFile:
+                data = json.load(varFile)
+                data[variableName] = float(result)
+                with open(varFilePath, "w") as varFile:
+                    json.dump(data, varFile)
+        else:
+            data = {}
+            data[variableName] = float(result)
+            with open(varFilePath, "w") as varFile:
+                json.dump(data, varFile)
     except:
         pass
 
@@ -150,8 +209,9 @@ def format_result(result):
 
 def calculate(query):
     results = []
-    # filter any special characters at start or end
+    base_query = query
     query = handle_trim_specials(query)
+    query = handle_assign(query)
     query = handle_factorials(query)
     query = handle_pow_xor(query)
     query = handle_implied_multiplication(query)
@@ -164,7 +224,7 @@ def calculate(query):
                                 '{} = {}'.format(query, result),
                                 'icons/app.png',
                                 'change_query',
-                                [str(result)],
+                                [str(result), base_query, variableName],
                                 True))
     except NameError:
         # try to find docstrings for methods similar to query
@@ -191,10 +251,10 @@ class Calculator(Wox):
     def query(self, query):
         return calculate(query)
 
-    def change_query(self, query):
+    def change_query(self, query, base_query, variableName):
         # change query and copy to clipboard after pressing enter
         WoxAPI.change_query(query)
-        write_to_x(query)
+        write_to_vars(query, variableName)
         copy_to_clipboard(query)
 
     def change_query_method(self, query):
